be1948f60390db00c1f493c7c1603ddac1234d7a
NickYoung <124875256+NickYoung-PanoramicData@users.noreply.github.com>
2023-02-23 15:31:23 +0000

    Moved implementation to repository

---
+++ CSharpToDo.Data/Interfaces/IToDosRepository.cs
@@ -0,0 +1,23 @@
+﻿using CSharpToDo.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CSharpToDo.Data.Interfaces
+{
+    public interface IToDosRepository
+    {
+		Task<IEnumerable<ToDo>> GetListAsync();
+
+		Task<ToDo?> GetAsync(int id);
+
+		Task<ToDo> AddAsync(ToDo item);
+
+		Task<ToDo?> UpdateAsync(int id,ToDo item);
+
+		Task<bool> DeleteAsync(int id);
+	}
+}
---
+++ CSharpToDos.Repositories.InMemory/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using CSharpToDo.Data.Interfaces;
+using CSharpToDos.Repositories.InMemory;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static IServiceCollection AddInMemoryRepository(this IServiceCollection services)
+		{
+			services.AddScoped<IToDosRepository, ToDosRepository>();
+			return services;
+		}
+	}
+}
