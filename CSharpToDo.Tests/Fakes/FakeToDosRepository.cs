c0b090992973cbf82bec56b2ad483aff05864932
NickYoung <124875256+NickYoung-PanoramicData@users.noreply.github.com>
2023-03-01 16:57:04 +0000

    Started work on incomplete fake ToDosRepository

---
+++ CSharpToDo.Tests/Fakes/FakeToDosRepository.cs
@@ -0,0 +1,23 @@
+﻿using CSharpToDo.Data.Interfaces;
+using CSharpToDo.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpToDo.Tests.Fakes
+{
+	internal class FakeToDosRepository : IToDosRepository
+	{
+		public Task<IEnumerable<ToDo>> GetListAsync(CancellationToken cancellationToken) => throw new NotImplementedException();
+
+		public Task<ToDo?> GetAsync(int id, CancellationToken cancellationToken) => throw new NotImplementedException();
+
+		public Task<ToDo> AddAsync(ToDo item, CancellationToken cancellationToken) => throw new NotImplementedException();
+
+		public Task<ToDo?> UpdateAsync(int id, ToDo item, CancellationToken cancellationToken) => throw new NotImplementedException();
+
+		public Task<bool> DeleteAsync(int id, CancellationToken cancellationToken) => throw new NotImplementedException();
+	}
+}
