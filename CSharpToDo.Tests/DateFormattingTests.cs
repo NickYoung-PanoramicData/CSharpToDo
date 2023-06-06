187c74a989e0f64da20712739faff0c5d77e8511
Nick Young <nick.young@panoramicdata.com>
2023-06-06 14:18:45 +0100

    Moved date display logic to an external file and added unit tests for it

---
+++ CSharpToDo.Tests/DateFormattingTests.cs
@@ -0,0 +1,88 @@
+﻿using CSharpToDo.Shared.Utilities;
+using FluentAssertions;
+
+namespace CSharpToDo.Tests;
+
+public class DateFormattingTests
+{
+	[Fact]
+	//Take an expired date that is marked as expirable and return a string that starts with "(Expired)"
+	public void GetFormattedDate_ExpirableAndExpired_ReturnsExpiredLabel()
+	{
+		DateTime inputDate = new DateTime(2023, 1, 1);
+		bool expirable = true;
+		DateTime comparisonDate = new DateTime(2023, 1, 2);
+
+		string result = DateDisplayLogic.GetFormattedDate(inputDate, expirable, comparisonDate);
+
+		result.Should().StartWith("(Expired)");
+	}
+
+	[Fact]
+	//Take a non-expired date that is marked as expirable and return a string that does not start with "(Expired)"
+	public void GetFormattedDate_ExpirableAndNotExpired_ReturnsFormattedDate()
+	{
+		DateTime inputDate = new DateTime(2023, 1, 2);
+		bool expirable = true;
+		DateTime comparisonDate = new DateTime(2023, 1, 1);
+
+		string result = DateDisplayLogic.GetFormattedDate(inputDate, expirable, comparisonDate);
+
+		result.Should().NotStartWith("(Expired)");
+		result.Should().Contain(inputDate.ToString("HH:mm"));
+	}
+
+	[Fact]
+	//Take a non-expirable date and return a string that does not start with "(Expired)"
+	public void GetFormattedDate_NonExpirable_ReturnsFormattedDate()
+	{
+		DateTime inputDate = new DateTime(2023, 1, 2);
+		bool expirable = false;
+		DateTime comparisonDate = new DateTime(2023, 1, 1);
+
+		string result = DateDisplayLogic.GetFormattedDate(inputDate, expirable, comparisonDate);
+
+		result.Should().NotStartWith("(Expired)");
+		result.Should().Contain(inputDate.ToString("HH:mm"));
+	}
+
+	[Fact]
+	//Take today's date and return a string that contains "Today at" and the time
+	public void GetFormattedDate_Today_ReturnsTodayFormattedDate()
+	{
+		DateTime inputDate = new DateTime(2023, 1, 1);
+		bool expirable = true;
+		DateTime comparisonDate = new DateTime(2023, 1, 1);
+		string result = DateDisplayLogic.GetFormattedDate(inputDate, expirable, comparisonDate);
+
+		result.Should().Contain("Today at");
+		result.Should().Contain(inputDate.ToString("HH:mm"));
+	}
+
+	[Fact]
+	//Take a date from the current year and return a string that contains the date and time
+	public void GetFormattedDate_CurrentYear_ReturnsFormattedDate()
+	{
+		DateTime inputDate = new DateTime(2023, 6, 1);
+		bool expirable = true;
+		DateTime comparisonDate = new DateTime(2023, 1, 1);
+
+		string result = DateDisplayLogic.GetFormattedDate(inputDate, expirable, comparisonDate);
+
+		result.Should().Contain("1st Jun");
+		result.Should().Contain(inputDate.ToString("HH:mm"));
+	}
+
+	[Fact]
+	//Take a date from a previous year and return a string that specifies the year and contains the date and time
+	public void GetFormattedDate_OldYear_ReturnsFormattedDate()
+	{
+		DateTime inputDate = new DateTime(2020, 1, 1);
+		bool expirable = true;
+
+		string result = DateDisplayLogic.GetFormattedDate(inputDate, expirable);
+
+		result.Should().Contain("Expired");
+		result.Should().Contain("2020");
+	}
+}
---
+++ Shared/Utilities/DateDisplayLogic.cs
@@ -0,0 +1,59 @@
+﻿namespace CSharpToDo.Shared.Utilities
+{
+	public static class DateDisplayLogic
+	{
+		public static string GetFormattedDate(DateTime inputDate, bool expirable)
+		{
+			return GetFormattedDate(inputDate, expirable, DateTime.Now);
+		}
+
+		internal static string GetFormattedDate(DateTime inputDate, bool expirable, DateTime comparisonDate)
+		{
+			string formattedDate = string.Empty;
+
+			if (expirable && inputDate < comparisonDate)
+			{
+				formattedDate = "(Expired) ";
+			}
+
+			if (inputDate.Date == comparisonDate.Date)
+			{
+				formattedDate += $"Today at {inputDate:HH:mm}";
+			}
+			else if (inputDate.Year == comparisonDate.Year)
+			{
+				formattedDate += $"{GetDayWithSuffix(inputDate.Day)} {inputDate:MMM HH:mm}";
+			}
+			else
+			{
+				formattedDate += $"{GetDayWithSuffix(inputDate.Day)} {inputDate:MMM yyyy HH:mm}";
+			}
+
+			return formattedDate;
+		}
+
+		private static string GetDayWithSuffix(int day)
+		{
+			string suffix;
+
+			if (day >= 11 && day <= 13)
+			{
+				suffix = "th";
+			}
+			else
+			{
+				int lastDigit = day % 10;
+
+				suffix = lastDigit switch
+				{
+					1 => "st",
+					2 => "nd",
+					3 => "rd",
+					_ => "th",
+				};
+			}
+
+			return $"{day}{suffix}";
+		}
+	}
+}
