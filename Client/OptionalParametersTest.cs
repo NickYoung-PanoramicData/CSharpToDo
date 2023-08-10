a78b61bb5763c4c672387140941afbb57bd0a8a4
Nick Young <nick.young@panoramicdata.com>
2023-08-10 11:00:36 +0100

    OPS-27724 - Attempting to produce issue

--- Client/OptionalParametersTest.cs
+++ Client/OptionalParametersTest.cs
@@ -2,4 +2,11 @@
 
 public class OptionalParametersTest
 {
+	public string OptionalParameters(string par1, string par2, string par3 = "test")
+	{
+		Console.WriteLine(par1);
+		Console.WriteLine(par2);
+		Console.WriteLine(par3);
+		return par1 + par2 + par3;
+	}
 }
