d6b471edcea728951bd81611b7c058bee98a4958
Nick Young <nick.young@panoramicdata.com>
2023-08-10 12:01:02 +0100

    OPS-27724 - Seeing if removing the lines will reintroduce the issue

--- Client/OptionalParametersTest.cs
+++ Client/OptionalParametersTest.cs
@@ -2,20 +2,18 @@
 
 public class OptionalParametersTest
 {
-	//Test comments to change the line numbers of methods
-	//Test comments to change the line numbers of methods
+
 	public string OptionalParameters(int par1)
 	{
 		Console.WriteLine(par1);
 		return par1.ToString();
 	}
-	//Test comments to change the line numbers of methods
-	//Test comments to change the line numbers of methods
-	//Test comments to change the line numbers of methods
+
 	public string OptionalParameters(string par1, string par2, string par3 = "test")
 	{
 		Console.WriteLine(par1);
 		Console.WriteLine(par2);
+		Console.WriteLine(par3);
 		return par1 + par2 + par3;
 	}
 }
