40af07d682bb56832e3cf2b67b5948b49c84b8f5
Nick Young <nick.young@panoramicdata.com>
2023-08-10 13:56:11 +0100

    OPS-27724 - Seeing if changing the return type will reintroduce the issue

--- Client/OptionalParametersTest.cs
+++ Client/OptionalParametersTest.cs
@@ -3,17 +3,17 @@
 public class OptionalParametersTest
 {
 
-	private string OptionalParametersTestMethod(int par1)
+	private int OptionalParametersTestMethod(int par1)
 	{
 		Console.WriteLine(par1);
-		return par1.ToString();
+		return par1;
 	}
 
-	private string OptionalParametersTestMethod(string par1, string par2, string par3 = "testString")
+	private int OptionalParametersTestMethod(string par1, string par2, string par3 = "testString")
 	{
 		Console.WriteLine(par1);
 		Console.WriteLine(par2);
 		Console.WriteLine(par3);
-		return par1 + par2 + par3;
+		return 5;
 	}
 }
