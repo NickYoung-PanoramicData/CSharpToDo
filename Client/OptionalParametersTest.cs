1a6c64e492c1aa8b02856acef9639aaa84946e2e
Nick Young <nick.young@panoramicdata.com>
2023-08-10 11:54:47 +0100

    OPS-27724 - Added comments to change method line number, to see if it will reintroduce the issue

--- Client/OptionalParametersTest.cs
+++ Client/OptionalParametersTest.cs
@@ -2,11 +2,16 @@
 
 public class OptionalParametersTest
 {
+	//Test comments to change the line numbers of methods
+	//Test comments to change the line numbers of methods
 	public string OptionalParameters(int par1)
 	{
 		Console.WriteLine(par1);
 		return par1.ToString();
 	}
+	//Test comments to change the line numbers of methods
+	//Test comments to change the line numbers of methods
+	//Test comments to change the line numbers of methods
 	public string OptionalParameters(string par1, string par2, string par3 = "test")
 	{
 		Console.WriteLine(par1);
